cf50c573e0d7631971bb4362fa33587c777882e9
danichol <daneel.nichol@mymona.uwi.edu>
2020-11-01 20:18:55 -0500

    Changes

--- scripts.js
+++ scripts.js
@@ -1,18 +1,19 @@
 /* Add your JavaScript to this file */
-var newsletter = function(){
-    const email = document.getElementsByID();
-    var input = document.getElementById("email");
-    var emailaddy = document.getElementById("email");
 
-    addEventListener('submit', (e) =>{
-        if (input==="" || input=== null){
-            document.getElementByClassName("message")[0].innerHTML ="Please enter a valid email address.";
-            e.preventDefault();
+var emailform = function(){
+    var entry = document.getElementById("email");
+    var emailaddy = document.getElementById("email");
+    document.getElementsByClassName("btn")[0].onclick=funtion(d);
+        var submission = entry.value;
+        var submission = emailaddy.value;
+        var empty = "";
+        if (entry===empty || entry===null){
+            document.getElementsByClassName("message")[0].innerHTML ="Please enter a valid email address.";
         }
         else{
-            document.getElementsByClassName("message")[0].innerHTML = "Thank you! Your email address" + enter +"has been added to our mailing list"
+            document.getElementsByClassName("message")[0].innerHTML = "Thank you! Your email address" + submission +"has been added to our mailing list"
         }
-      
-    }
+        d.preventDefault();
 }
-window.onload=newsletter;
+
+window.onload = emailform;
