9bb243dd0f5e8b8495c05f142f3c4de4ed44e36d
danichol <daneel.nichol@mymona.uwi.edu>
2020-11-01 22:42:08 -0500

    changes

--- scripts.js
+++ scripts.js
@@ -2,25 +2,19 @@
 
 var emailform = function(){
     var entry = document.getElementById("email");
-    /* var emailaddy = document.getElementById("email"); */
-
-    /* document.getElementById("myAnchor").addEventListener("click", function(e){
-        e.preventDefault()
-      });
-    
-    */
+   
     document.getElementsByClassName("btn")[1].addEventListener("click", function(e){
-    var input = entry.value;
-    /* var submission = emailaddy.value; */ 
-            if (input==="" || input===null){
+    var submission = entry.value;
+    
+            if (submission==="" || submission===null){
                 document.getElementsByClassName("message")[0].innerHTML ="Please enter a valid email address.";
             }
-            else{
 
-                document.getElementsByClassName("message")[0].innerHTML = "Thank you! Your email address " + input +" has been added to our mailing list"
+            else{
+                document.getElementsByClassName("message")[0].innerHTML = "Thank you! Your email address " + submission +" has been added to our mailing list"
             }
+
             e.preventDefault();})
-        }
-    
+        }    
 
 window.onload=emailform;
